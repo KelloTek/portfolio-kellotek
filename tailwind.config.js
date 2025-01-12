22dcb8280dd1fd6cfa484bccc26299a143e1106b
KelloTek <cldauge@gmail.com>
2025-01-12 18:24:13 +0100

    v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [ "./*.{html,js}" ],
+  darkMode: 'selector',
   theme: {
     extend: {
       colors: {
         "custom-black": "#1f1f1f",
         "custom-gray": "#b6bac0",
-        "custom-blue": "#0ea5e9",
-        "custom-blue-2": "#5263ff",
+        "custom-gray-2": "#334155",
+        "custom-blue": "#38bdf8",
+        "custom-blue-2": "#38bdf81a",
       }
     },
   },
